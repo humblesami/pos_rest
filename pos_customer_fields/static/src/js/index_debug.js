cd8135c9bbf1e1c59852a04699601c83ac346bd2
sami92news <sami.akram@92newshd.tv>
2022-03-10 10:31:02 +0500

    now searching customer by vehicle number

--- pos_customer_fields/static/src/js/index_debug.js
+++ pos_customer_fields/static/src/js/index_debug.js
@@ -1,4 +1,4 @@
-odoo.define('pos_customer_fields',function(require) {
+odoo.define('pos_customer_fields.customer_fields',function(require) {
     "use strict";
     var models = require('point_of_sale.models');
     var screens = require('point_of_sale.screens');
@@ -19,19 +19,51 @@ odoo.define('pos_customer_fields',function(require) {
         },
     });
 
-    // not working
-//    var posDB = require('point_of_sale.DB');
-//    var posDBSearch =  posDB.extend({
-//        _partner_search_string: function(partner){
-//            console.log('partner search');
-//            this._super();
-//        }
-//    });
+    var posDB_super = require('point_of_sale.DB');
+    var posDBSearch =  posDB_super.include({
+        _partner_search_string: function(partner){
+            console.log('partner search');
+            let str = '';
+            if(partner.vehicle_number)
+            {
+                str = original_partner_search_string(partner);
+                str += '|' + partner.vehicle_number;
+                str = '' + partner.id + ':' + str.replace(':', '').replace(/\n/g, ' ') + '\n';
+            }
+            else{
+                str = this._super(partner);
+            }
+            return str;
+        }
+    });
+
+    function original_partner_search_string(partner){
+        var str =  partner.name;
+        if(partner.barcode){
+            str += '|' + partner.barcode;
+        }
+        if(partner.address){
+            str += '|' + partner.address;
+        }
+        if(partner.phone){
+            str += '|' + partner.phone.split(' ').join('');
+        }
+        if(partner.mobile){
+            str += '|' + partner.mobile.split(' ').join('');
+        }
+        if(partner.email){
+            str += '|' + partner.email;
+        }
+        if(partner.vat){
+            str += '|' + partner.vat;
+        }
+        return str;
+    }
 //    return posDBSearch;
-//    var ClientListScreenWidget = screens.ClientListScreenWidget.extend({
+//    var ClientListScreenWidget = screens.ClientListScreenWidget.include({
 //        save_client_details: function(partner){
 //            console.log('client saved');
-//            this._super();
+//            return this._super();
 //        }
 //    });
 });
