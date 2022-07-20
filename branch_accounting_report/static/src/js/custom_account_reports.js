6f3a8cd16bcd80030a212e788a609cd3b7e4333a
sami92news <sami.akram@92newshd.tv>
2022-07-20 15:51:02 +0500

    department selection along with partners

--- branch_accounting_report/static/src/js/custom_account_reports.js
+++ branch_accounting_report/static/src/js/custom_account_reports.js
@@ -5,11 +5,10 @@ odoo.define('branch_accounting_report.account_report_generic', function (require
         render_searchview_buttons: function () {
             let self = this;
             let res = this._super();
-            console.log('v3 search view buttons', ' rendered');
-            self.$searchview_buttons.find('.js_branch_auto_complete').on('change', function () {
-                self.report_options.branch = self.$searchview_buttons.find('[data-filter="branch"]').val();
-                //self.report_options.analytic_tags = self.$searchview_buttons.find('[data-filter="analytic_tags"]').val();
-                console.log("self.report_options.branch called", self.report_options.branch);
+            let branch_dom = self.$searchview_buttons.find('.js_branch_auto_complete');
+            branch_dom.select2().val(self.report_options.branch).trigger("change");
+            branch_dom.on('change', function () {
+                self.report_options.branch = branch_dom.val();
                 return self.reload().then(function () {
                     self.$searchview_buttons.find('.account_branch_filter').click();
                 })
